0477d9167c2bc52010a11ff6ee8f6658c41ebeab
Grisha <grigorije05@gmail.com>
2021-10-10 19:50:40 +0200

    fix toast for older systems

--- client/toast.py
+++ client/toast.py
@@ -1,11 +1,18 @@
-from win10toast import ToastNotifier
-
 class MyncNotifier:
     def __init__(self, controller) -> None:
+        try:
+            from win10toast import ToastNotifier
+            self.initialized = True
+        except:
+            self.initialized = False
+            return
+
         self.controller = controller
         self.toast = ToastNotifier()
+        
 
     def notify(self, content, title="Mync"):
+        if not self.initialized: return
         self.toast.show_toast(
             title,
             content,
